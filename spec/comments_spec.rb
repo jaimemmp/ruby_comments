9b56e0e614cfdb559f13aa714bf931407c992034
Jaime Muñoz <jaimemmp@gmail.com>
2013-11-06 12:25:44 +0100

    cambio de parejas

--- spec/comments_spec.rb
+++ spec/comments_spec.rb
@@ -7,4 +7,11 @@ describe "related comments sistem" do
 		comments = Comments.new(1,"awesome",1,1)
 		expect(comments.count_comments(1)).to eq(1)
 	end
+	it "Calc distance betwen 2 users" do
+		comments = Comments.new(1,"awesome",444,1)
+		comments = Comments.new(2,"awesome too",555,1)
+
+		distance = user.calc_distance(444,555)
+		expect(distance).to eq(0)
+	end
 end
