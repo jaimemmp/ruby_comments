5ba741d565c8826ac72ed109c993f161e300279a
Jaime Muñoz <jaimemmp@gmail.com>
2013-11-06 12:58:15 +0100

    ultimo commit antes de añadir funcionalidades

--- lib/Comments.rb
+++ lib/Comments.rb
@@ -13,6 +13,11 @@ class Comments
 		@@comments << self
 	end
 
+	def self.empty ()
+		@@coments = []
+	end
+
+
 	def count_comments(movie_id)
 		result = 0
 		@@comments.each do |comment|
@@ -22,4 +27,28 @@ class Comments
 		end
 		result
 	end
+	def calc_distance(user_id_1,user_id_2)
+
+		@@comments.each do |comment|
+			if comment.user_id == user_id_1
+				if search_more_comments(comment.movie_id,user_id_2)
+					return 0
+				else
+					return 1
+				end
+			end
+		end
+	end
+
+	def search_more_comments(movie_id,user_id)
+		result = false
+		@@comments.each do |comment|
+			if (comment.movie_id == movie_id && comment.user_id == user_id)
+				result = true
+			end
+		end
+		result
+	end
+
+
 end
--- spec/comments_spec.rb
+++ spec/comments_spec.rb
@@ -3,15 +3,30 @@ require_relative "../lib/Users"
 require_relative "../lib/Comments"
 
 describe "related comments sistem" do
+	before :each do 
+		Comments.empty
+	end
+
 	it "post a comment" do
 		comments = Comments.new(1,"awesome",1,1)
 		expect(comments.count_comments(1)).to eq(1)
 	end
-	it "Calc distance betwen 2 users" do
+
+	it "Calc distance betwen 2 users with distance 0" do
 		comments = Comments.new(1,"awesome",444,1)
 		comments = Comments.new(2,"awesome too",555,1)
 
-		distance = user.calc_distance(444,555)
+		distance = comments.calc_distance(444,555)
 		expect(distance).to eq(0)
+	end
+
+	it "Calc distance betwen 2 users with distance 1" do
+		comments = Comments.new(1,"awesome",444,1)
+		comments = Comments.new(2,"awesome too",555,1)
+		comments = Comments.new(3,"awesome too",555,2)
+		comments = Comments.new(4,"awesome too",333,2)
+
+		distance = comments.calc_distance(444,333)
+		expect(distance).to eq(1)
 	end
 end
